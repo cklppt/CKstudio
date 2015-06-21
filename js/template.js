1788516ac3e43823d4ca1157e6fbb757d3ddda28
unknown <CKL@PC-Q11.(none)>
2015-06-21 23:29:05 +0800

    Add all the countries and cities on the mapgit add -A! Huge work.

--- js/template.js
+++ js/template.js
@@ -124,6 +124,99 @@
 
 
 		// Map
+
+		countries = ['BE',// 	Belgium 1
+				 	'JP',// 	Japan 2
+				 	'LU',// 	Luxembourg 3
+					'GR',// 	Greece 4
+					'GB',// 	United Kingdom 5
+					'HU',// 	Hungary 6
+					'EE',// 	Estonia 7
+					'ES',// 	Spain 8
+					'MY',// 	Malaysia 9
+					'FR',// 	France 10
+					'FI',// 	Finland 11
+					'NL',// 	Netherlands 12
+					'NO',// 	Norway 13
+					'NZ',// 	New Zealand 14
+					'CH',// 	Switzerland 15
+					'CN',// 	China 16
+					'CZ',// 	Czech Rep 17
+					'SE',// 	Sweden 18
+					'DE',// 	Germany 19
+					'AT',// 	Austria 20
+					'US',// 	United States 21
+					'TW',// 	Taiwan 22
+					'AE',// 	United Arab Emirates 23
+					'IT',// 	Italy 24
+					'AU',// 	Australia 25
+					'IE',// 	Ireland 26
+					'VA',//     Vatican 27
+					'SG'//		Singapore 28
+					];
+		cities = [
+			{latLng: [50.850340, 4.35171], name: 'Bruxelles'},
+			{latLng: [35.689487, 139.691706], name: 'Tokyo'},
+			{latLng: [35.011636, 135.768029], name: 'Kyoto'},
+			{latLng: [49.815273, 6.129583], name: 'Luxembourg'},
+			{latLng: [37.983917, 23.72936], name: 'Athen'},
+			{latLng: [39.721391, 21.633613], name: 'Meteora'},
+			{latLng: [36.393156, 25.461509], name: 'Santorini'},
+			{latLng: [51.507351, -0.127758], name: 'London'},
+			{latLng: [52.205337, 0.121817], name: 'Cambridge'},
+			{latLng: [47.497912, 19.040235], name: 'Budapest'},
+			{latLng: [59.436961, 24.753575], name: 'Tallinn'},
+			{latLng: [39.469907, -0.376288], name: 'Valencia'},
+			{latLng: [40.416775, -3.70379], name: 'Madrid'},
+			{latLng: [40.070392, -2.137416], name: 'Cuenca'},
+			{latLng: [41.385064, 2.173403], name: 'Barcelona'},
+			{latLng: [3.139003, 101.686855], name: 'Kuala Lumpur'},
+			{latLng: [48.856614, 2.352222], name: 'Paris'},
+			{latLng: [45.764043, 4.835659], name: 'Lyon'},
+			{latLng: [48.636063, -1.511457], name: 'Mont Saint Michel'},
+			{latLng: [60.173324, 24.941025], name: 'Helsinki'},
+			{latLng: [52.370216, 4.895168], name: 'Amsterdam'},
+			{latLng: [50.851368, 5.690973], name: 'Maastricht'},
+			{latLng: [52.740178, 6.077978], name: 'Giethoorn'},
+			{latLng: [58.969976, 5.733107], name: 'Stavanger'},
+			{latLng: [-37.787001, 175.279253], name: 'Hamilton'},
+			{latLng: [46.947922, 7.444608], name: 'Bern'},
+			{latLng: [47.050038, 8.308929], name: 'Luzern'},
+			{latLng: [25.606486, 100.267638], name: 'Dali'},
+			{latLng: [29.714748, 118.337621], name: 'Huangshan'},
+			{latLng: [30.274085, 120.15507], name: 'Hangzhou'},
+			{latLng: [27.829743, 99.700836], name: 'Shangrila'},
+			{latLng: [50.075538, 14.4378], name: 'Praha'},
+			{latLng: [48.812735, 14.317466], name: 'Cesky Krumlov'},
+			{latLng: [59.329323, 18.068581], name: 'Stockholm'},
+			{latLng: [50.737430, 7.098207], name: 'Bonn'},
+			{latLng: [50.775346, 6.083887], name: 'Aachen'},
+			{latLng: [48.135125, 11.581981], name: 'Munich'},
+			{latLng: [48.208174, 16.373819], name: 'Wien'},
+			{latLng: [47.809490, 13.05501], name: 'Salzburg'},
+			{latLng: [37.774929, -122.419416], name: 'San Francisco'},
+			{latLng: [37.871593, -122.272747], name: 'Berkeley'},
+			{latLng: [25.032969, 121.565418], name: 'Taipei'},
+			{latLng: [22.627278, 120.301435], name: 'Kaohsiung'},
+			{latLng: [25.204849, 55.270783], name: 'Dubai'},
+			{latLng: [41.902783, 12.496366], name: 'Rome'},
+			{latLng: [43.769560, 11.255814], name: 'Firenze'},
+			{latLng: [45.440847, 12.315515], name: 'Venezia'},
+			{latLng: [38.115688, 13.361267], name: 'Palermo'},
+			{latLng: [-31.953513, 115.857047], name: 'Perth'},
+			{latLng: [-35.023823, 117.884729], name: 'Albany'},
+			{latLng: [53.349805, -6.26031], name: 'Dublin'},
+			{latLng: [41.902916, 12.453389], name: 'Vatican City'},
+			{latLng: [1.352083, 103.819836], name: 'Singapore'},
+			{latLng: [22.396428, 114.109497], name: 'Hong Kong'},
+			{latLng: [50.937531, 6.960279], name: 'Cologne'},
+			{latLng: [51.227741, 6.773456], name: 'Dusseldorf'},
+			{latLng: [55.953252, -3.188267], name: 'Edinburgh'},
+			{latLng: [34.693738, 135.502165], name: 'Osaka'},			
+			{latLng: [36.719903, 139.698216], name: 'Nikko'},
+			{latLng: [35.232355, 139.106937], name: 'Hakone'}
+			];
+
 		$('#world-map').vectorMap({
 			map: 'world_mill_en',
 			scaleColors: ['#C8EEFF', '#0071A4'],
@@ -136,38 +229,16 @@
 					stroke: '#383f47'
 				}
 			},
+			regionStyle: {
+				selected: {
+    				fill: '#339BEB'
+			  	}
+			},
 			zoomOnScroll: false,
 			backgroundColor: 'rgba(0, 0, 0, 0)',
-			markers: [
-			{latLng: [41.90, 12.45], name: 'Vatican City'},
-			{latLng: [43.73, 7.41], name: 'Monaco'},
-			{latLng: [-0.52, 166.93], name: 'Nauru'},
-			{latLng: [-8.51, 179.21], name: 'Tuvalu'},
-			{latLng: [43.93, 12.46], name: 'San Marino'},
-			{latLng: [47.14, 9.52], name: 'Liechtenstein'},
-			{latLng: [7.11, 171.06], name: 'Marshall Islands'},
-			{latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
-			{latLng: [3.2, 73.22], name: 'Maldives'},
-			{latLng: [35.88, 14.5], name: 'Malta'},
-			{latLng: [12.05, -61.75], name: 'Grenada'},
-			{latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
-			{latLng: [13.16, -59.55], name: 'Barbados'},
-			{latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
-			{latLng: [-4.61, 55.45], name: 'Seychelles'},
-			{latLng: [7.35, 134.46], name: 'Palau'},
-			{latLng: [42.5, 1.51], name: 'Andorra'},
-			{latLng: [14.01, -60.98], name: 'Saint Lucia'},
-			{latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
-			{latLng: [1.3, 103.8], name: 'Singapore'},
-			{latLng: [1.46, 173.03], name: 'Kiribati'},
-			{latLng: [-21.13, -175.2], name: 'Tonga'},
-			{latLng: [15.3, -61.38], name: 'Dominica'},
-			{latLng: [-20.2, 57.5], name: 'Mauritius'},
-			{latLng: [26.02, 50.55], name: 'Bahrain'},
-			{latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}
-			]
+			selectedRegions : countries,
+			markers: cities
 		});
-
 
 	}); // End document ready
 })(this.jQuery);
